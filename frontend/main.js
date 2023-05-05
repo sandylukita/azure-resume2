7d32099e7b0b155b90994a3f97091aad41357f79
unknown <sandylukita@gmail.com>
2023-05-06 00:14:42 +0700

    Created Workflows

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 } )
+
+const functionApiUrl = 'https://getresumecountersl.azurewebsites.net/api/GetResumeCounter?code=fkHvDb8kkeaLD391lkjoZYpAVPmLEN5DAarwegiiAyD0AzFuyu8iFw=='
 const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response =>{
+    fetch(functionApiUrl).then(response =>{
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
